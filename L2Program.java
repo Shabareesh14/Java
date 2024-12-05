0795c6ab0796901ef12f004487ba9703d786af1a
Shabareesh14 <140871007+Shabareesh14@users.noreply.github.com>
2024-12-05 16:01:28 +0530

    Update L2Program.java

--- L2Program.java
+++ L2Program.java
@@ -1,4 +1,4 @@
-// Swap Two Numbers
+												// Swap Two Numbers
 /*class L2Program{
 	public static void main(String[] args){
 		int a = 10;
@@ -13,16 +13,42 @@
 	}
 }*/
 
-//Fibanocci In Recursion
+												//Fibanocci In Recursion
+// class L2Program{
+// 	public static int fibanocci(int n){
+// 		if(n <= 1) return n;
+// 		return fibanocci(n - 1) + fibanocci(n - 2);
+// 	}
+// 	public static void main(String[] args){
+// 		int n = 10;
+// 		for(int i = 0; i < n; i++){
+// 			System.out.println(fibanocci(i) + " ");
+// 		}
+// 	}
+// }
+
+												//Distinct Character and their Count
 class L2Program{
-	public static int fibanocci(int n){
-		if(n <= 1) return n;
-		return fibanocci(n - 1) + fibanocci(n - 2);
-	}
 	public static void main(String[] args){
-		int n = 10;
-		for(int i = 0; i < n; i++){
-			System.out.println(fibanocci(i) + " ");
+		String str = "abcdABCDabcd";
+		
+		StringBuilder build = new StringBuilder();
+		for(int i = 0; i < str.length(); i++){
+			char currentChar = str.charAt(i);
+			
+			if(build.indexOf(String.valueOf(currentChar)) != -1){
+				continue;
+			}
+			
+			int count = 0;
+			for(int j = 0; j < str.length(); j++){
+				if(str.charAt(j) == currentChar){
+					count++;
+				}
+			}
+			build.append(currentChar);
+			
+			System.out.println(currentChar +" : "+count);
 		}
 	}
 }
